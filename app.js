39de6f6d01a988c3be3c444e09f4b0d2107a096d
Tyler Bushnell <tyler@interlincx.com>
2014-10-23 15:40:17 -0700

    fix syntax

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/', function(req, res){
 app.get('/services', function(req, res){
   services = JSON.parse(process.env.VCAP_SERVICES || "{}");
   if( typeof(services['mysql-5.5']) != 'undefined' ){
-    res.send( services['mysql-5.5.'] );
+    res.send( services['mysql-5.5'] );
   } else {
     res.send( services );
   }

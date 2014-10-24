98865bada930623434a23b9f3be724371e128373
Tyler Bushnell <tyler@interlincx.com>
2014-10-23 17:52:22 -0700

    testsave

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.post('/api/documents/:name', function(req, res){
   data = {};
   data.handle = req.params.name;
   data.content = sent.markdown;
-  conn.updateDocument(data, function(err, result){
+  conn.saveDocument(data, function(err, result){
     if( err ){
       res.send(err);
     } else {
